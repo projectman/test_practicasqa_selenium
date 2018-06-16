09777a95ca48658266e3b72b11fef1e9c1c84ce0
projectman <obushmelev@gmail.com>
2018-06-16 15:07:01 -0500

    Login passed and opend next page found

--- features/step_definitions/login.feature
+++ features/step_definitions/login.feature
@@ -3,7 +3,7 @@ Feature: User with valid credentials can log in
   Scenario: Pass registration form when username and password are correct
     Given I open the brouser with authorisation page
     When I imput correct username "admin" and correct password "admin"
-    Then I can download the page with text "SQA practics"
+    Then I can download the page with text "Practical SQA"
 
 
 
--- features/step_definitions/my_steps_authorisation.rb
+++ features/step_definitions/my_steps_authorisation.rb
@@ -1,25 +1,41 @@
 url = "http://old.practicalsqa.net"
+username_path = "//input[@id='user_login']"
 
 Given(/^I open the brouser with authorisation page$/) do
 
-  browser = Selenium::WebDriver.for :safari
-  browser.navigate.to url
+  @browser = Selenium::WebDriver.for :safari
+  @browser.navigate.to url
 
 
   #element = browser.find_element(name: 'q')
   #element.send_keys "Hello WebDriver!"
   #element.submit
 
-  puts "title of the brouser: " + browser.title
+  puts "title of the brouser: " + @browser.title
 
-  sleep(10)
-  browser.quit
+
 
 end
 
-When(/^I imput correct username "([^"]*)" and correct password "([^"]*)"$/) do
+When(/^I imput correct username "([^"]*)" and correct password "([^"]*)"$/) do |user, password|
+  puts "username and password: "+user+" "+password
+
+  @browser.find_element(xpath: username_path).send_keys user
+  @browser.find_element(xpath: "//input[@id='user_pass']").send_keys password
+  @browser.find_element(xpath: "//input[@id='wp-submit']").click
+
+
+  sleep(5)
+
 end
 
-Then(/^I can download the page with text "([^"]*)"$/) do |arg|
-  pending
+
+Then(/^I can download the page with text "([^"]*)"$/) do |head_text|
+  puts "argument for search: "+head_text
+  xpath_t = "//a[text()='"+head_text+"']"
+
+  #xpath_t = "//a[text()='Practical SQA']"
+  puts "xpath_t: "+xpath_t
+
+  puts "@brouser result: ", @browser.find_element(xpath: xpath_t)
 end
