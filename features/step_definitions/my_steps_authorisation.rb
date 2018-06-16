ff1418b1433fc99d9663a7b74f4a7460832a12f0
projectman <obushmelev@gmail.com>
2018-06-16 15:24:01 -0500

    Improved code with enter and find 2nd page

--- features/step_definitions/my_steps_authorisation.rb
+++ features/step_definitions/my_steps_authorisation.rb
@@ -1,29 +1,24 @@
 url = "http://old.practicalsqa.net"
 username_path = "//input[@id='user_login']"
+pass_path = "//input[@id='user_pass']"
+button_path = "//input[@id='wp-submit']"
+
 
 Given(/^I open the brouser with authorisation page$/) do
 
   @browser = Selenium::WebDriver.for :safari
   @browser.navigate.to url
-
-
-  #element = browser.find_element(name: 'q')
-  #element.send_keys "Hello WebDriver!"
-  #element.submit
-
+  # Check that username field exists on page
+  @browser.find_element(xpath: username_path)
   puts "title of the brouser: " + @browser.title
-
-
 
 end
 
-When(/^I imput correct username "([^"]*)" and correct password "([^"]*)"$/) do |user, password|
-  puts "username and password: "+user+" "+password
+When(/^I imput correct username "([^"]*)" and correct password "([^"]*)"$/) do |user, pwrd|
 
   @browser.find_element(xpath: username_path).send_keys user
-  @browser.find_element(xpath: "//input[@id='user_pass']").send_keys password
-  @browser.find_element(xpath: "//input[@id='wp-submit']").click
-
+  @browser.find_element(xpath: pass_path).send_keys pwrd
+  @browser.find_element(xpath: button_path).click
 
   sleep(5)
 
@@ -31,11 +26,6 @@ end
 
 
 Then(/^I can download the page with text "([^"]*)"$/) do |head_text|
-  puts "argument for search: "+head_text
-  xpath_t = "//a[text()='"+head_text+"']"
 
-  #xpath_t = "//a[text()='Practical SQA']"
-  puts "xpath_t: "+xpath_t
-
-  puts "@brouser result: ", @browser.find_element(xpath: xpath_t)
+  puts "@brouser result: ", @browser.find_element(xpath: "//a[text()='"+head_text+"']" )
 end
